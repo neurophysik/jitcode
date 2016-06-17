cf7f9168a185fb8f2652958bac30221c100a0991
Wrzlprmft <bla@example.org>
2016-06-17 16:57:36 +0200

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 	url = 'http://github.com/neurophysik/jitcode',
 	packages = ['jitcode'],
 	package_data = {'jitcode': ['jitced_template.c']},
-	include_package_date = True,
+	include_package_data = True,
 	install_requires = requirements,
 	setup_requires = ['setuptools_scm'],
 	use_scm_version = {'write_to': 'jitcode/version.py'},
