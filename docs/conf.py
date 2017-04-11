7e8e7102704a54d5f0e692154ba8cbf863a72d9a
Wrzlprmft <bla@example.org>
2017-04-11 13:43:57 +0200

    Nicer mocks.

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,11 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.modules['scipy.integrate'] = Mock(ode=object)
 
+class Symbol(object):
+	def __init__(*args, **kwargs):
+		pass
+sys.modules['sympy'] = Mock(Function=Symbol, Symbol=Symbol)
+
 sys.path.insert(0,os.path.abspath("../examples"))
 sys.path.insert(0,os.path.abspath("../jitcode"))
 
