15e0b8a9400633056197b82d8dc043b44553e7db
Gerrit Ansmann <bla@example.com>
2017-10-27 11:34:19 +0200

    Fixing test for restricted lyaps to avoid butterfly effect.

--- tests/test_restricted_lyap.py
+++ tests/test_restricted_lyap.py
@@ -4,46 +4,57 @@
 from jitcode import jitcode_restricted_lyap, y
 import numpy as np
 from scipy.stats import sem
+from symengine import Symbol
 
-a  = -0.025794
-b  =  0.01
-c  =  0.02
+a = -0.025794
+b =  0.01
+c =  0.02
+k = Symbol("k")
+
+f = [
+		y(0)*(a-y(0))*(y(0)-1.0) - y(1) + k*(y(2)-y(0)),
+		b*y(0) - c*y(1),
+		y(2)*(a-y(2))*(y(2)-1.0) - y(3) + k*(y(0)-y(2)),
+		b*y(2) - c*y(3)
+	]
+
+vectors = [
+		np.array([1.,0.,1.,0.]),
+		np.array([0.,1.,0.,1.])
+	]
+
+ODE = jitcode_restricted_lyap( f, vectors=vectors, verbose=False, control_pars=[k] )
+ODE.generate_f_C(simplify=False)
+ODE.set_integrator("dopri5")
 
 scenarios = [
-		{"k": 0.128, "sign":-1},
 		{"k":-0.128, "sign": 1},
+		{"k":-0.2  , "sign": 1},
+		{"k": 0.128, "sign":-1},
+		{"k": 0.2  , "sign":-1},
 		{"k": 0    , "sign": 0},
 	]
 
 for scenario in scenarios:
-	f = [
-		y(0)*(a-y(0))*(y(0)-1.0) - y(1) + scenario["k"]*(y(2)-y(0)),
-		b*y(0) - c*y(1),
-		y(2)*(a-y(2))*(y(2)-1.0) - y(3) + scenario["k"]*(y(0)-y(2)),
-		b*y(2) - c*y(3)
-		]
-	
+	ODE.set_f_params(scenario["k"])
+
 	if scenario["sign"]<0:
 		initial_state = np.random.random(4)
 	else:
 		single = np.random.random(2)
 		initial_state = np.hstack([single,single])
-	
-	vectors = [
-		np.array([1.,0.,1.,0.]),
-		np.array([0.,1.,0.,1.])
-		]
-	
-	ODE = jitcode_restricted_lyap( f, vectors=vectors, verbose=False )
-	ODE.set_integrator("dopri5")
 	ODE.set_initial_value(initial_state,0.0)
 	
-	data = np.hstack(ODE.integrate(T)[1] for T in range(10,100000,10))
+	times = range(10,100000,10)
+	data = np.hstack(ODE.integrate(time)[1] for time in times)
+	
+	assert ODE.y[0]==ODE.y[2]
+	assert ODE.y[1]==ODE.y[3]
 	
 	lyap = np.average(data[500:])
 	margin = sem(data[500:])
 	sign = 0 if abs(lyap)<margin else np.sign(lyap)
-	assert sign==scenario["sign"], "Test failed in scenario %s"%scenario
+	assert sign==scenario["sign"], "Test failed in scenario %s. (Lyapunov exponent: %f±%f)" % (scenario,lyap,margin)
 	print(".",end="",flush=True)
 
 print("")
