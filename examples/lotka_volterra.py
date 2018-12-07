7e21c41180a6e957ae7a1388652142d7c499f3b4
Gerrit Ansmann <bla@example.com>
2018-12-07 19:56:37 +0100

    Updating Lotka–Volterra example to use dictionaries throughout.

--- examples/lotka_volterra.py
+++ examples/lotka_volterra.py
@@ -34,7 +34,7 @@ The `y` that we imported from `jitcode` has to be used for the dynamical variabl
 	:lines: 9
 	:dedent: 1
 
-We here might as well have written `R,B = y(0),y(1)`, but the above is more handy for larger systems. Note that the following code is written such that the order of our dynamical variables only matters for the output.
+We here might as well have written `R,B = y(0),y(1)`, but the above is more handy for larger systems. Note that the following code is written such that the order of our dynamical variables does not matter.
 
 We implement the differential equation as a dictionary, mapping each of the dynamical variables to its derivative:
 
@@ -52,32 +52,32 @@ Now, we have everything, we need to instantiate `jitcode`, i.e., to initialise t
 	:lines: 16
 	:dedent: 1
 
-Before we start integrating, we have to choose an integrator and define initial conditions. We here choose the 5th-order Dormand–Prince method, random initial states (between 0 and 1) and start at :math:`t=0`.
+Before we start integrating, we have to choose an integrator and define initial conditions. We here choose the 5th-order Dormand–Prince method. Moreover the initial :math:`B` shall be 0.5, the initial :math:`R` shall be 0.2, and the starting time should be :math:`t=0`.
 
 .. literalinclude:: ../examples/lotka_volterra.py
 	:start-after: example-st\u0061rt
 	:lines: 17-19
 	:dedent: 1
 
-We then define an array of time points where we want to observe the system and an empty list that shall be filled with our results.
+We then define an array of time points where we want to observe the system, and a dictionary of empty lists that shall be filled with our results.
 
 .. literalinclude:: ../examples/lotka_volterra.py
 	:start-after: example-st\u0061rt
 	:lines: 21-22
 	:dedent: 1
 
-Finally, we perform the actual integration by calling `ode.integrate` for each of our `times`. This returns the state of the system after integration, which we put into a tuple together with the current time and append to our `data` list. (The asterisk (*) unpacks an iterable. We might as well have written `[time,state[0],state[1]]` instead of `[time,*state]`.)
+Finally, we perform the actual integration by calling `ODE.integrate` for each of our `times`. After this, we use `ODE.y_dict` to access the current state as a dictionary and appending its values to the respective lists.
 
 .. literalinclude:: ../examples/lotka_volterra.py
 	:start-after: example-st\u0061rt
-	:lines: 23-25
+	:lines: 23-26
 	:dedent: 1
 
 We can now plot or analyse our data, but that’s beyond the scope of JiTCODE. So we just save it:
 
 .. literalinclude:: ../examples/lotka_volterra.py
 	:start-after: example-st\u0061rt
-	:lines: 27
+	:lines: 28
 	:dedent: 1
 
 Taking everything together, our code is:
@@ -98,7 +98,7 @@ if __name__ == "__main__":
 	ω = 0.5
 	ν = 0.5
 	
-	R,B = [ y(i) for i in range(2) ]
+	R,B = dynvars = [ y(i) for i in range(2) ]
 	
 	lotka_volterra_diff = {
 			B:  γ*B - φ*R*B,
@@ -107,13 +107,14 @@ if __name__ == "__main__":
 	
 	ODE = jitcode(lotka_volterra_diff)
 	ODE.set_integrator("dopri5")
-	initial_state = np.array(np.random.random(2))
+	initial_state = { R: 0.2, B: 0.5 }
 	ODE.set_initial_value(initial_state,0.0)
 	
 	times = np.arange(0.0,100,0.1)
-	data = []
+	values = { R: [], B: [] }
 	for time in times:
-		state = ODE.integrate(time)
-		data.append( [time,*state] )
+		ODE.integrate(time)
+		for dynvar in dynvars:
+			values[dynvar].append(ODE.y_dict[dynvar])
 	
-	np.savetxt("timeseries.dat", data)
+	np.savetxt("timeseries.dat",np.vstack((values[R],values[B])).T)
