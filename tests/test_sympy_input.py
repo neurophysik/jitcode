b20995902a8bf8870c92ce671a21a06d17cd86ed
Gerrit Ansmann <bla@example.com>
2019-06-01 10:50:52 +0200

    Improving Symbol origin test to cover all sorts of imports.

--- tests/test_sympy_input.py
+++ tests/test_sympy_input.py
@@ -1,23 +1,57 @@
 """
-Tests whether everything works with a pure SymPy input.
+Tests whether things works independent of where symbols are imported from.
 """
 
-from jitcode import jitcode
+import jitcode
+import jitcode.sympy_symbols
 import sympy
 import symengine
 
-results = []
+symengine_manually = [
+		symengine.Symbol("t",real=True),
+		symengine.Function("y",real=True),
+		symengine.cos,
+	]
 
-for backend in [sympy,symengine]:
-	t = backend.Symbol("t", real=True)
-	y = backend.Function("y")
-	cos = backend.cos
-	
-	ODE = jitcode( [cos(t)*y(0)], verbose=False )
+sympy_manually = [
+		sympy.Symbol("t",real=True),
+		sympy.Function("y",real=True),
+		sympy.cos,
+	]
+
+jitcode_provisions = [
+		jitcode.t,
+		jitcode.y,
+		symengine.cos,
+	]
+
+jitcode_sympy_provisions = [
+		jitcode.sympy_symbols.t,
+		jitcode.sympy_symbols.y,
+		symengine.cos,
+	]
+
+mixed = [
+		jitcode.sympy_symbols.t,
+		jitcode.y,
+		sympy.cos,
+	]
+
+results = set()
+
+for t,y,cos in [
+			symengine_manually,
+			sympy_manually,
+			jitcode_provisions,
+			jitcode_sympy_provisions,
+			mixed
+		]:
+	ODE = jitcode.jitcode( [cos(t)*y(0)], verbose=False )
 	ODE.set_integrator("dopri5")
 	ODE.set_initial_value([1.0],0.0)
 	
 	result = ODE.integrate(10)[0]
-	results.append(result)
+	results.add(result)
 
-assert results[0]==results[1]
+assert len(results)==1
+
